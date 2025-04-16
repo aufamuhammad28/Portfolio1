89233cf2d4204696efd04614e9b758fbc8dd1869
aufamuhammad28 <aufamuhammad28@gmail.com>
2025-04-16 16:34:38 +0800

    Add files via upload

---
+++ sql_total_sales_cte.sql
@@ -0,0 +1,30 @@
+SELECT 
+	 cust_name,
+	 order_date,
+	 order_status,
+	 owner_id
+FROM 
+	 `double-reef-187606.aafiyatdb.smart_ordering` as so
+RIGHT JOIN 
+	 `double-reef-187606.aafiyatdb.ownership_journey` as oj
+	ON 
+	  so.CUST_ID = oj.customer_id
+WHERE 
+	 order_date = '2023-09-30 12:59:17.000';
+
+
+
+WITH monthly_sales_cte AS (
+    SELECT 
+        DATE_TRUNC(CAST(month AS DATE), MONTH) AS sales_month, 
+        SUM(MTD_SALES) AS total_monthly_sales
+    FROM 
+        `double-reef-187606.aafiyatdb.month sales hourly record`
+    GROUP BY 
+        sales_month
+)
+SELECT 
+    sales_month, 
+    total_monthly_sales
+FROM 
+    monthly_sales_cte;
